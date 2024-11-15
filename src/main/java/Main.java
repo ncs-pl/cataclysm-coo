5fc06de9beb9eba0e2dc12abaa80448b98f49be1
nc0 <n@nc0.fr>
2024-11-15 23:59:25 +0100

    Création de l'application et de sa game loop

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,21 @@
+import controleur.Controleur;
+import modele.EtatJeu;
+import vue.Ihm;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Ihm ihm = new Ihm(System.in, System.out, System.err);
+        Controleur controleur = new Controleur(ihm);
+
+        ihm.afficherInformation("Bievenue dans l'application du jeu d'aventure sur terminal !");
+        controleur.initialiserJeu();
+
+        while (controleur.getEtatJeu() != EtatJeu.TERMINE) {
+            controleur.jouerTour();
+            controleur.afficherJeu();
+        }
+
+        controleur.terminerJeu();
+        ihm.afficherInformation("Fin de l'application, au revoir.");
     }
 }
