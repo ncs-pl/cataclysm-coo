0aafeaad607a0516ced2ae6604dca8ac5fe9b8a2
nc0 <n@nc0.fr>
2024-11-27 12:29:13 +0100

    Passage du lifecycle dans le contrôleur
    C'est apparament la version MVC de la prof...

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,12 @@
 import controleur.Controleur;
-import modele.EtatJeu;
 import vue.Ihm;
 
 public class Main {
     public static void main(String[] args) {
         Ihm ihm = new Ihm(System.in, System.out, System.err);
         Controleur controleur = new Controleur(ihm);
-
         ihm.afficherInformation("Bievenue dans l'application du jeu d'aventure sur terminal !");
-        controleur.initialiserJeu();
-
-        while (controleur.getEtatJeu() != EtatJeu.TERMINE) {
-            controleur.afficherCarte();
-            controleur.jouerTour();
-        }
-
-        controleur.terminerJeu();
+        controleur.jouer();
         ihm.afficherInformation("Fin de l'application, au revoir.");
     }
 }
--- src/main/java/controleur/Controleur.java
+++ src/main/java/controleur/Controleur.java
@@ -15,32 +15,17 @@ import java.util.List;
 public class Controleur {
     /** Interface homme-machine pour la communication du jeu. */
     private final Ihm ihm;
-
     /** Une partie de jeu. */
     private Jeu jeu;
-    /** État de la partie de jeu en cours. */
-    private EtatJeu etatJeu;
+
+    private boolean enCours = false;
 
     public Controleur(Ihm ihm) {
         assert(ihm != null);
         this.ihm = ihm;
     }
 
-    /**
-     * Retourne l'état de la partie de jeu en cours.
-     * @return l'état de la partie.
-     */
-    public EtatJeu getEtatJeu() {
-        return this.etatJeu;
-    }
-
-    /**
-     * Créer et initialise une nouvelle partie de jeu, en chargeant ou créant une carte et en choisissant
-     * le thème de la partie.
-     */
-    public void initialiserJeu() {
-        assert(this.etatJeu == EtatJeu.AUCUNE);
-
+    private void initialiserJeu() {
         /// Préparation de la carte ///
 
         // TODO(nico): système de carte fonctionnel avec une vraie classe de manipulation.
@@ -100,16 +85,12 @@ public class Controleur {
         }
 
         /// Initialisation du jeu ///
-        ihm.afficherInformation(carte.toString()); // Méthode utilisée uniquement pour le débogage
+        this.ihm.afficherInformation(carte.toString()); // Méthode utilisée uniquement pour le débogage
         this.jeu = new Jeu(theme, carte);
-        this.etatJeu = EtatJeu.EN_COURS;
+        this.enCours = true;
     }
 
-    /** Affiche l'état du jeu en cours (la carte etc...). */
     private void afficherCarte() {
-        assert(this.etatJeu != EtatJeu.AUCUNE);
-        assert(this.jeu != null);
-
         JeuTheme theme = this.jeu.getTheme();
         StringBuilder affichage = new StringBuilder();
         List<List<Acteur>> carte = jeu.getCarte();
@@ -193,131 +174,128 @@ public class Controleur {
         this.ihm.afficherMessageBrut(jeu.toString()); // Méthode utilisée uniquement pour le débogage
     }
 
-    /** Affiche les résultats de la partie terminée et dé-initialise le tout. */
-    private void terminerJeu() {
-        assert(this.etatJeu == EtatJeu.TERMINE);
-        assert(this.jeu != null);
-
-        // TODO(nico): afficher les résultats de la partie terminée.
-    }
-
-    /** Récupère les entrées utilisateurs pour jouer un tour de la partie en cours. */
-    public void jouerTour() {
-        assert (this.etatJeu == EtatJeu.EN_COURS);
-        assert (this.jeu != null);
-
-        // TODO(nico): demander les actions à effectuer au joueur et les appliquer au jeu.
-        // TODO(nico): après les actions du tour, vérifier si la partie est finie ou non, et potentiellement
-        //             modifier this.etatJeu sur EtatJeu.TERMINE.
-
-        //Question Lucas : Ajouter les menus et action possible dans la classe du personnage joueur ?
+    private void executerInstruction() {
         boolean choixInstruction = true;
         while (choixInstruction) {
             String instruction = this.ihm.demanderString("Entrez une instruction.");
             switch (instruction.toLowerCase()) {
-                case "aide", "a", "?":
-                    String aide = "Manuel d'utilisation de Cataclysm COO :\n";
-                    aide += "\n";
-                    aide += "* aide (a, ?):\n";
-                    aide += "\tAffiche ce manuel\n";
-                    aide += "\n";
-                    aide += "* quitter (q):\n";
-                    aide += "\tTermine la partie.\n";
-                    aide += "\n";
-                    aide += "* carte (c):\n";
-                    aide += "\tAffiche la carte du jeu.\n";
-                    aide += "\n";
-                    aide += "* haut, bas, gauche, droite (h, b, g, d):\n";
-                    aide += "\tDéplace le joueur vers le haut, le bas, la gauche ou la droite respectivement.\n";
-                    aide += "\n";
-                    aide += "* ramasser haut, bas, gauche, droite (rh, rb, rg, rd):\n";
-                    aide += "\tRamasse l'objet de la case du haut, du bas, de gauche ou de droite respectivement.\n";
-                    aide += "\n";
+            case "aide", "a", "?":
+                String aide = "\nManuel d'utilisation de Cataclysm COO :\n";
+                aide += "\n";
+                aide += "* aide (a, ?):\n";
+                aide += "\tAffiche ce manuel\n";
+                aide += "\n";
+                aide += "* quitter (q):\n";
+                aide += "\tTermine la partie.\n";
+                aide += "\n";
+                aide += "* carte (c):\n";
+                aide += "\tAffiche la carte du jeu.\n";
+                aide += "\n";
+                aide += "* haut, bas, gauche, droite (h, b, g, d):\n";
+                aide += "\tDéplace le joueur vers le haut, le bas, la gauche ou la droite respectivement.\n";
+                aide += "\n";
+                aide += "* ramasser haut, bas, gauche, droite (rh, rb, rg, rd):\n";
+                aide += "\tRamasse l'objet de la case du haut, du bas, de gauche ou de droite respectivement.\n";
+                aide += "\n";
+                this.ihm.afficherInformation(aide);
 
-                    this.ihm.demanderString(aide);
+                choixInstruction = false;
+                break;
 
+            case "quitter", "q":
+                this.enCours = false;
+                choixInstruction = false;
+                break;
+
+            case "carte", "c":
+                this.afficherCarte();
+                choixInstruction = false;
+                break;
+
+            // Déplacements
+            case "haut", "h":
+                try {
+                    this.jeu.deplacerJoueur(Position.HAUT);
                     choixInstruction = false;
-                    break;
-                case "quitter", "q":
-                    this.terminerJeu();
+                } catch (DeplacementImpossibleException e) {
+                    this.ihm.afficherErreur(e.getMessage());
+                }
+                break;
+            case "bas", "b":
+                try {
+                    this.jeu.deplacerJoueur(Position.BAS);
                     choixInstruction = false;
-                    break;
-                case "carte", "c":
-                    this.afficherCarte();
+                } catch (DeplacementImpossibleException e) {
+                    this.ihm.afficherErreur(e.getMessage());
+                }
+                break;
+            case "gauche", "g":
+                try {
+                    this.jeu.deplacerJoueur(Position.GAUCHE);
                     choixInstruction = false;
-                    break;
+                } catch (DeplacementImpossibleException e) {
+                    this.ihm.afficherErreur(e.getMessage());
+                }
+                break;
+            case "droite", "d":
+                try {
+                    this.jeu.deplacerJoueur(Position.DROITE);
+                    choixInstruction = false;
+                } catch (DeplacementImpossibleException e) {
+                    this.ihm.afficherErreur(e.getMessage());
+                }
+                break;
 
-                // Déplacements
-                case "haut", "h":
-                    try {
-                        this.jeu.deplacerJoueur(Position.HAUT);
-                        choixInstruction = false;
-                    } catch (DeplacementImpossibleException e) {
-                        this.ihm.afficherErreur(e.getMessage());
-                    }
-                    break;
-                case "bas", "b":
-                    try {
-                        this.jeu.deplacerJoueur(Position.BAS);
-                        choixInstruction = false;
-                    } catch (DeplacementImpossibleException e) {
-                        this.ihm.afficherErreur(e.getMessage());
-                    }
-                    break;
-                case "gauche", "g":
-                    try {
-                        this.jeu.deplacerJoueur(Position.GAUCHE);
-                        choixInstruction = false;
-                    } catch (DeplacementImpossibleException e) {
-                        this.ihm.afficherErreur(e.getMessage());
-                    }
-                    break;
-                case "droite", "d":
-                    try {
-                        this.jeu.deplacerJoueur(Position.DROITE);
-                        choixInstruction = false;
-                    } catch (DeplacementImpossibleException e) {
-                        this.ihm.afficherErreur(e.getMessage());
-                    }
-                    break;
+            // Ramasser
+            case "ramasser haut", "ra":
+                try {
+                    this.jeu.ramasserObjet(Position.HAUT);
+                    choixInstruction = false;
+                } catch (AucunObjetException e) {
+                    this.ihm.afficherErreur(e.getMessage());
+                }
+                break;
+            case "ramasser bas", "rb":
+                try {
+                    this.jeu.ramasserObjet(Position.BAS);
+                    choixInstruction = false;
+                } catch (AucunObjetException e) {
+                    this.ihm.afficherErreur(e.getMessage());
+                }
+                break;
+            case "ramasser gauche", "rg":
+                try {
+                    this.jeu.ramasserObjet(Position.GAUCHE);
+                    choixInstruction = false;
+                } catch (AucunObjetException e) {
+                    this.ihm.afficherErreur(e.getMessage());
+                }
+                break;
+            case "ramasser droite", "rd":
+                try {
+                    this.jeu.ramasserObjet(Position.DROITE);
+                    choixInstruction = false;
+                } catch (AucunObjetException e) {
+                    this.ihm.afficherErreur(e.getMessage());
+                }
+                break;
 
-                // Ramasser
-                case "ramasser haut", "ra":
-                    try {
-                        this.jeu.ramasserObjet(Position.HAUT);
-                        choixInstruction = false;
-                    } catch (AucunObjetException e) {
-                        this.ihm.afficherErreur(e.getMessage());
-                    }
-                    break;
-                case "ramasser bas", "rb":
-                    try {
-                        this.jeu.ramasserObjet(Position.BAS);
-                        choixInstruction = false;
-                    } catch (AucunObjetException e) {
-                        this.ihm.afficherErreur(e.getMessage());
-                    }
-                    break;
-                case "ramasser gauche", "rg":
-                    try {
-                        this.jeu.ramasserObjet(Position.GAUCHE);
-                        choixInstruction = false;
-                    } catch (AucunObjetException e) {
-                        this.ihm.afficherErreur(e.getMessage());
-                    }
-                    break;
-                case "ramasser droite", "rd":
-                    try {
-                        this.jeu.ramasserObjet(Position.DROITE);
-                        choixInstruction = false;
-                    } catch (AucunObjetException e) {
-                        this.ihm.afficherErreur(e.getMessage());
-                    }
-                    break;
-                default:
-                    ihm.afficherErreur("Instruction invalide, tapez 'aide' pour consulter le manuel.");
-                    break;
+            default:
+                ihm.afficherErreur("Instruction invalide, tapez \"aide\" pour consulter le manuel.");
+                break;
             }
         }
+    }
+
+    public void jouer() {
+        this.initialiserJeu();
+        while (this.enCours) {
+            // TODO(nico): notifier le jeu qu'on débute un tour
+            this.afficherCarte();
+            this.executerInstruction();
+            // TODO(nico): notifier le jeu qu'on fini un tour
+        }
+
+        // TODO(nico): fin du jeu
     }
 }
