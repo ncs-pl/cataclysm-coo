75362130c5f37c79b3a4ed9e4e51ad702e3fc509
nc0 <n@nc0.fr>
2024-11-28 10:08:17 +0100

    Compression du code logique du jeu

--- src/main/java/modele/Jeu.java
+++ src/main/java/modele/Jeu.java
@@ -8,19 +8,54 @@ public class Jeu {
     private final List<List<Acteur>> carte;
 
     private Personnage personnage;
-    private List<Objet> objets;
-    private List<Animal> animaux;
-    private List<Acteur> decors;
-    private int tourCourant;
+    private final List<Objet> objets = new ArrayList<Objet>();
+    private final List<Animal> animaux = new ArrayList<Animal>();
+    private final List<Acteur> decors = new ArrayList<Acteur>();
 
     public Jeu(JeuTheme theme, List<List<Acteur>> carte) {
         this.theme = theme;
         this.carte = carte;
-        this.objets = new ArrayList<>();
-        this.animaux = new ArrayList<>();
-        this.decors = new ArrayList<>();
-        this.tourCourant = 1;
-        this.initialiserJeu();
+
+        // Initialisation du jeu
+        for (List<Acteur> ligne : this.carte) {
+            for (Acteur acteur : ligne) {
+                switch (theme){
+                case FORET:
+                    switch (acteur.id) {
+                    case PERSONNAGE:
+                        this.personnage = (Personnage) acteur;
+                        break;
+                    case ARBRE:
+                        this.decors.add((Arbre)acteur);
+                        break;
+                    case BUISSON:
+                        this.decors.add((Buisson)acteur);
+                        break;
+                    case GLAND:
+                        this.objets.add((Gland)acteur);
+                        break;
+                    case CHAMPIGNON:
+                        this.objets.add((Champignon)acteur);
+                        break;
+                    case ECUREUIL:
+                        this.animaux.add((Ecureuil)acteur);
+                        break;
+                    default:
+                        break;
+                    }
+                    break;
+
+                case JUNGLE:
+                    assert(false); // TODO(nico): faire le thème Jungle
+                    break;
+
+                default:
+                    // TODO(nico): Ajouter une meilleure de gestion d'erreur
+                    //             dans le parsing de carte?
+                    assert(false); // Thème invalide
+                }
+            }
+        }
     }
 
     public JeuTheme getTheme() {
@@ -31,219 +66,149 @@ public class Jeu {
         return carte;
     }
 
-    public void initialiserJeu(){
-        for(List<Acteur> ligne : this.carte){
-            for(Acteur acteur : ligne){
-                switch (theme){
-                    case FORET :{
-                        switch (acteur.id){
-                            case PERSONNAGE: {
-                                this.personnage = (Personnage) acteur;
-                                break;
-                            }
-                            case ARBRE: {
-                                this.decors.add((Arbre)acteur);
-                                break;
-                            }
-                            case BUISSON:{
-                                this.decors.add((Buisson) acteur);
-                                break;
-                            }
-                            case GLAND:{
-                                this.objets.add((Gland) acteur);
-                                break;
-                            }
-                            case CHAMPIGNON:{
-                                this.objets.add((Champignon) acteur);
-                                break;
-                            }
-                            case ECUREUIL:{
-                                this.animaux.add((Ecureuil) acteur);
-                                break;
-                            }
-                            default : {
-                                break;
-                            }
-                        }
-                    }
-                    case JUNGLE: {
-                        // unimplemented
-                        assert(false);
-                    }
-                    break;
-                    default: {
-                        // unreachable
-                        assert(false);
-                    } break;
-                }
+    // TODO(nico): vérification autrement des coordonnées ?
+    // TODO(nico): indexer les cellules à partir de 1 et cacher en interne le
+    //             fait de faire -1?
 
-            }
-
-        }
+    private boolean verifierCoordonneesCellule(int x, int y) {
+        return (x >= 0 && x < carte.get(0).size()) || (y >= 0 && y < carte.size());
     }
 
-/**
- * Affiche des informations détaillées sur l'état actuel du programme
- * à des fins de débogage. Cette méthode est principalement utilisée
- * pour identifier et corriger les problèmes pendant le développement.
- */
-
- // METHODES DE TEST (A supprimer) //
-
-    public String afficherObjets(){
-        String affichage = "Objets du jeu \n";
-        for(Objet o : this.objets){
-            affichage += o.id + "(" + o.getX() + ", " + o.getY() + ") | ";
-        }
-        affichage += "\n";
-        return affichage;
-    }
-    public String afficherAnimaux(){
-        String affichage = "Animaux du jeu : \n";
-        for(Animal a : this.animaux){
-            affichage += a.id + "(" + a.getX() + ", " + a.getY() + ") | ";
-        }
-        affichage += "\n";
-        return affichage;
-    }
-    public String afficherDecors(){
-        String affichage = "Decors de la carte : \n";
-        for(Acteur d : this.decors){
-            affichage += d.id + "(" + d.getX() + ", " + d.getY() + ") | ";
-        }
-        affichage += "\n";
-        return affichage;
+    private Acteur getCellule(int x, int y) {
+        assert(this.verifierCoordonneesCellule(x, y));
+        return this.carte.get(y).get(x);
     }
 
-    public String afficherInventaire(){
-        String affichage = "Inventaire : \n";
-        for(Objet o : this.personnage.getInventaire()){
-            affichage += o.id + " | " ;
-        }
-        affichage += "\n";
-        return affichage;
+    private void setCellule(int x, int y, Acteur acteur) {
+        assert(this.verifierCoordonneesCellule(x, y));
+        this.carte.get(y).set(x, acteur);
     }
 
-    @Override
-    public String toString() {
-        return "Game State : \n" +
-                this.afficherObjets() +
-                this.afficherAnimaux() +
-                this.afficherDecors() +
-                "Personnage : (" + personnage.getX() + ", " + personnage.getY() + ")\n" +
-                this.afficherInventaire();
-    }
-
-    // FIN METHODES DE TEST (A supprimer) //
-
-    public void deplacerJoueur(Position position) throws DeplacementImpossibleException{
+    public void deplacerJoueur(Position position) throws DeplacementImpossibleException {
         int x = 0;
         int y = 0;
 
         switch (position) {
-            case HAUT:
-                y -= 1;
-                break;
-            case BAS:
-                y += 1;
-                break;
-            case DROITE:
-                x += 1;
-                break;
-            case GAUCHE:
-                x -= 1;
-                break;
+        case HAUT:
+            y -= 1;
+            break;
+        case BAS:
+            y += 1;
+            break;
+        case DROITE:
+            x += 1;
+            break;
+        case GAUCHE:
+            x -= 1;
+            break;
+        default:
+            assert(false); // Variant d'énumération invalide
+            break;
         }
 
-        if ( notDansLaCarte(this.personnage.getX() + x, this.personnage.getY() + y) ){
-            throw new DeplacementImpossibleException("Bordure de carte");
-        }
-        if ( decors.contains(carte.get(this.personnage.getY()+y).get(this.personnage.getX()+x))){
+        int nouveauX = this.personnage.getX() + x;
+        int nouveauY = this.personnage.getY() + y;
+
+        if (!this.verifierCoordonneesCellule(nouveauX, nouveauY))
+            throw new DeplacementImpossibleException("Bordure de carte.");
+
+        Acteur celluleVisee = this.getCellule(nouveauX, nouveauY);
+        // TODO(nico): faire la vérification selon l'ID de l'Acteur obtenu
+        //             plutôt que de faire des opérations longues de
+        //             recherche en boucle...
+        if (decors.contains(celluleVisee))
             throw new DeplacementImpossibleException("Le passage est bloqué.");
-        }
-        if (animaux.contains(carte.get(this.personnage.getY()+y).get(this.personnage.getX()+x))){
-            throw new DeplacementImpossibleException("Un animal est sur votre chemin ,détournez-le.");
-        }
-        if (objets.contains(carte.get(this.personnage.getY()+y).get(this.personnage.getX()+x))){
-            throw new DeplacementImpossibleException("Un objet bloque le passage , ramassez-le ou changez de direction.");
-        }
-        changerPositionActeur(this.personnage.getX()+x, this.personnage.getY()+y, this.personnage);
+        if (animaux.contains(celluleVisee))
+            throw new DeplacementImpossibleException("Un animal est sur votre chemin.");
+        if (objets.contains(celluleVisee))
+            throw new DeplacementImpossibleException("Un objet bloque le passage.");
+
+        // Déplacer le personnage.
+        int ancienX = this.personnage.getX();
+        int ancienY = this.personnage.getY();
+
+        this.personnage.setX(nouveauX);
+        this.personnage.setY(nouveauY);
+        this.setCellule(x, y, this.personnage);
+
+        this.setCellule(ancienX, ancienY, new CaseVide(ancienX, ancienY));
     }
 
-    private boolean notDansLaCarte(int x, int y) {
-        return x < 0 || y < 0 || y >= carte.size() || x >= carte.get(y).size();
-    }
-
-    private void changerPositionActeur(int x, int y, Acteur acteur){
-        int tempX = this.personnage.getX();
-        int tempY = this.personnage.getY();
-        this.personnage.setX(x);
-        this.personnage.setY(y);
-        carte.set(y,carte.get(y)).set(x, acteur);
-        carte.set(tempY, carte.get(tempY)).set(tempX, new CaseVide(tempX,tempY));
-    }
-
-    public boolean objetAutourJoueur(){
-        int pX = this.personnage.getX();
-        int pY = this.personnage.getY();
-
-        int[][] directions = {
-                {pX-1,pY},
-                {pX+1,pY},
-                {pX,pY-1},
-                {pX,pY+1},
-        };
-        for (int[] couple : directions){
-            if(!notDansLaCarte(couple[0],couple[1])) {
-                if (objets.contains(carte.get(couple[1]).get(couple[0]))) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    public void ramasserObjet(Position position) throws AucunObjetException{
+    public void ramasserObjet(Position position) throws AucunObjetException {
         //TODO(Younes) : Pour ramasserObjet , il faut gérer l'exception où le personnage est en bordure de carte
         //TODO(Younes) : Ajouter la possibilité de choisir l'objet à ramasser
         int x = 0;
         int y = 0;
 
         switch (position) {
-            case HAUT:
-                y -= 1;
-                break;
-            case BAS:
-                y += 1;
-                break;
-            case DROITE:
-                x += 1;
-                break;
-            case GAUCHE:
-                x -= 1;
-                break;
+        case HAUT:
+            y -= 1;
+            break;
+        case BAS:
+            y += 1;
+            break;
+        case DROITE:
+            x += 1;
+            break;
+        case GAUCHE:
+            x -= 1;
+            break;
+        default:
+            assert(false); // Variant d'énumération invalide
+            break;
         }
-
 
         int xObjet = this.personnage.getX() + x;
         int yObjet = this.personnage.getY() + y;
 
-        if (!notDansLaCarte(xObjet,yObjet)){
-            Acteur contenu = carte.get(yObjet).get(xObjet);
-            if (objets.contains(carte.get(yObjet).get(xObjet))){
-                this.personnage.getInventaire().add((Objet)contenu);
-                this.objets.remove(contenu);
-                carte.get(yObjet).set(xObjet,new CaseVide(xObjet,yObjet));
-            } else {
-                throw new AucunObjetException("Aucun objet ici");
-            }
-        } else {
-            throw new AucunObjetException("Aucun objet ici");
-        }
+        if (!this.verifierCoordonneesCellule(xObjet, yObjet))
+            throw new AucunObjetException("Aucun objet ici.");
 
+        Acteur contenu = this.getCellule(xObjet, yObjet);
+        // TODO(nico): comparer selon l'ID du contenu
+        if (!objets.contains(contenu))
+            throw new AucunObjetException("Aucun objet ici.");
+
+        this.personnage.getInventaire().add((Objet)contenu);
+        this.objets.remove(contenu);
+        this.setCellule(xObjet, yObjet, new CaseVide(xObjet, yObjet));
     }
 
-    //TODO(Younes) : Poser un objet
+    @Override public String toString() {
+        String affichage = "";
+        affichage += "Game State {\n";
 
+        affichage += "\tobjets:";
+        for (Objet objet : this.objets) {
+            //noinspection StringConcatenationInLoop
+            affichage += "\t\t" + objet.id + " (" + objet.getX() + ", " + objet.getY() + "),\n";
+        }
+        affichage += "\t]\n";
 
+        affichage += "\tanimaux:";
+        for (Animal animal : this.animaux) {
+            //noinspection StringConcatenationInLoop
+            affichage += "\t\t" + animal.id + " (" + animal.getX() + ", " + animal.getY() + "),\n";
+        }
+        affichage += "\t]\n";
+
+        affichage += "\tdecors:";
+        for (Acteur decor : this.decors) {
+            //noinspection StringConcatenationInLoop
+            affichage += "\t\t" + decor.id + " (" + decor.getX() + ", " + decor.getY() + "),\n";
+        }
+        affichage += "\t]\n";
+
+        affichage += "\tpersonnage: (" + this.personnage.getX() + ", " + this.personnage.getY() + "),\n";
+
+        affichage += "\tinventaire:";
+        for (Objet objet : this.personnage.getInventaire()) {
+            //noinspection StringConcatenationInLoop
+            affichage += "\t\t" + objet.id + ",\n";
+        }
+        affichage += "\t]\n";
+
+        affichage += "}\n";
+        return affichage;
+    }
 }
