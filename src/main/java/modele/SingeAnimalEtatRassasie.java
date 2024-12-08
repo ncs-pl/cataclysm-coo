c9c386170a3f7c4da2eeecb5ddfcde5e75e61e5a
nc0 <n@nc0.fr>
2024-12-08 21:40:17 +0100

    Ajout état RASSASIE singe

--- src/main/java/modele/SingeAnimalEtatRassasie.java
+++ src/main/java/modele/SingeAnimalEtatRassasie.java
@@ -14,15 +14,33 @@ public class SingeAnimalEtatRassasie extends AnimalEtat {
         return SingeAnimalEtatRassasie.instance;
     }
 
-    @Override public void deplacer(Animal animal) {
-        // TODO(nico)
+    @SuppressWarnings("DuplicatedCode")
+    @Override public void deplacer(Animal animal, Jeu jeu) {
+        int ligne   = animal.obtenirLigne();
+        int colonne = animal.obtenirColonne();
+        // On cherche la première cellule vide parmi celle du dessus,
+        // de gauche, de droite, ou d'en dessous (dans cet ordre), ou sinon
+        // on ne bouge pas.
+        if (jeu.verifierCaseVide(ligne - 1, colonne))     ligne   -= 1;
+        else if (jeu.verifierCaseVide(ligne,     colonne - 1)) colonne -= 1;
+        else if (jeu.verifierCaseVide(ligne,     colonne + 1)) colonne += 1;
+        else if (jeu.verifierCaseVide(ligne + 1, colonne))     ligne   += 1;
+        animal.changerLigne(ligne);
+        animal.changerColonne(colonne);
+
+        int saturation = animal.obtenirSaturation();
+        animal.changerSaturation(saturation - 1);
+
+        if (saturation == 0) {
+            animal.changerEtat(SingeAnimalEtatAffame.obtenirInstance());
+        }
     }
 
-    @Override public void manger(Animal animal) {
-        // TODO(nico)
+    @Override public void manger(Animal animal, Jeu jeu) {
+        throw new AnimalEtatException("Singe déjà rassasié...");
     }
 
     @Override public void prendreCoup(Animal animal) {
-        // TODO(nico)
+        throw new AnimalEtatException("Comportement non-spécifié.");
     }
 }
