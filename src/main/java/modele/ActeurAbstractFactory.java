88bf52001d75a00efb4f2a501f34716ec026a82c
nc0 <n@nc0.fr>
2025-01-05 14:37:49 +0100

    Petite correction

--- src/main/java/modele/ActeurAbstractFactory.java
+++ src/main/java/modele/ActeurAbstractFactory.java
@@ -1,59 +1,15 @@
 package modele;
 
 public interface ActeurAbstractFactory {
-    Animal creerAnimal(int ligne,
-                       int colonne,
-                       int maxLignes,
-                       int maxColonnes); // Créer un acteur animal.
-
-    Acteur creerDecor1(int ligne,
-                       int colonne,
-                       int maxLignes,
-                       int maxColonnes); // Créer un acteur décoratif.
-
-    Acteur creerDecor2(int ligne,
-                       int colonne,
-                       int maxLignes,
-                       int maxColonnes); // Créer un acteur décoratif.
-
-    Objet creerObjetAliment(int ligne,
-                            int colonne,
-                            int maxLignes,
-                            int maxColonnes); // Créer un acteur consommable
-
-    Objet creerObjetChampignon(int ligne,
-                               int colonne,
-                               int maxLignes,
-                               int maxColonnes); // Créer un acteur champignon
-
-    Objet creerObjetChampignonDrogue(int ligne,
-                                     int colonne,
-                                     int maxLignes,
-                                     int maxColonnes); // Créer un acteur champignon de la drogue
-
-    Predateur creerPredateur1(int ligne,
-                              int colonne,
-                              int maxLignes,
-                              int maxColonnes); // Créer le premier type de prédateur.
-
-    Predateur creerPredateur2(int ligne,
-                              int colonne,
-                              int maxLignes,
-                              int maxColonnes); // Créer le second type de prédateur.
-
-    Personnage creerPersonnage(int ligne,
-                               int colonne,
-                               int maxLignes,
-                               int maxColonnes); // Créer le personnage.
-
-    ZoneVide creerZoneVide(int ligne,
-                           int colonne,
-                           int maxLignes,
-                           int maxColonnes); // Créer une zone vide.
-
-    Acteur creerParSymbole(char symbole,
-                           int ligne,
-                           int colonne,
-                           int maxLignes,
-                           int maxColonnes); // Créer l'acteur selon le symbole Carte lu.
+    Animal     creerAnimal(int ligne, int colonne, int maxLignes, int maxColonnes);                   // Créer un acteur animal.
+    Acteur     creerDecor1(int ligne, int colonne, int maxLignes, int maxColonnes);                   // Créer un acteur décoratif.
+    Acteur     creerDecor2(int ligne, int colonne, int maxLignes, int maxColonnes);                   // Créer un acteur décoratif.
+    Objet      creerObjetAliment(int ligne, int colonne, int maxLignes, int maxColonnes);             // Créer un acteur consommable
+    Objet      creerObjetChampignon(int ligne, int colonne, int maxLignes, int maxColonnes);          // Créer un acteur champignon
+    Objet      creerObjetChampignonDrogue(int ligne, int colonne, int maxLignes, int maxColonnes);    // Créer un acteur champignon de la drogue
+    Predateur  creerPredateur1(int ligne, int colonne, int maxLignes, int maxColonnes);               // Créer le premier type de prédateur.
+    Predateur  creerPredateur2(int ligne, int colonne, int maxLignes, int maxColonnes);               // Créer le second type de prédateur.
+    Personnage creerPersonnage(int ligne, int colonne, int maxLignes, int maxColonnes);               // Créer le personnage.
+    ZoneVide   creerZoneVide(int ligne, int colonne, int maxLignes, int maxColonnes);                 // Créer une zone vide.
+    Acteur     creerParSymbole(char symbole, int ligne, int colonne, int maxLignes, int maxColonnes); // Créer l'acteur selon le symbole Carte lu.
 }
