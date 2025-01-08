2059663d83222ce00228226db0111d41daeb2e30
Paulo Lucas <lucas.paulo@etu.univ-orleans.fr>
2025-01-08 20:40:14 +0100

    Correction chargement carte

--- src/main/java/controleur/Controleur.java
+++ src/main/java/controleur/Controleur.java
@@ -90,7 +90,7 @@ public class Controleur {
         Carte carte = null;
         while (carte == null) { // TODO(nico): error handling
             String chemin = this.ihm.demanderString("Entrez le nom du fichier de la carte à utiliser, ou rien pour en créer une nouvelle.");
-            if(chemin.isEmpty()) carte = Carte.ouvrir(chemin);
+            if(!chemin.isEmpty()) carte = Carte.ouvrir(chemin);
             else {
                 // Création d'une carte manuellement
                 carte = new Carte();
