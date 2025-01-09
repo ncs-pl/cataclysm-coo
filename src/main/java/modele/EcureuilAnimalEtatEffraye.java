42be23174853fb59f1f22a448fa3629c755bafd7
Paulo Lucas <lucas.paulo@etu.univ-orleans.fr>
2025-01-09 01:24:25 +0100

    Correction bug Hibou -> Soucie provenant du changement d'état dans EcureuilAnimalEtatEffraye

--- src/main/java/modele/EcureuilAnimalEtatEffraye.java
+++ src/main/java/modele/EcureuilAnimalEtatEffraye.java
@@ -29,34 +29,26 @@ public class EcureuilAnimalEtatEffraye extends AnimalEtat {
         if(tourEffrayer.containsKey(animal)){
             tourEffrayer.put(animal, tourEffrayer.get(animal) - 1);
             if (tourEffrayer.get(animal) <= 0){
-                System.out.println("aled");
                 tourEffrayer.remove(animal);
                 if(animal.obtenirSaturation() == 0){
-                    if (jeu.verifierCaseDecors(ligne, colonne)){
-                        int type = jeu.obtenirCaseDecors(ligne, colonne).obtenirType();
-                        if(type == Acteur.TYPE_ARBRE){
-                            animal.changerEtat(new EcureuilAnimalEtatPerche(EcureuilAnimalEtatAffame.obtenirInstance()));
-                        } else if (type == Acteur.TYPE_BUISSON) {
-                            animal.changerEtat(new EcureuilAnimalEtatCache(EcureuilAnimalEtatAffame.obtenirInstance()));
-                        } else {
-                            animal.changerEtat(EcureuilAnimalEtatAffame.obtenirInstance());
-                        }
-                    } else {
-                        animal.changerEtat(EcureuilAnimalEtatAffame.obtenirInstance());
-                    }
+                    animal.changerEtat(EcureuilAnimalEtatAffame.obtenirInstance());
                 } else {
-                    animal.changerSaturation(animal.obtenirSaturation() - 1);
+                    animal.changerEtat(EcureuilAnimalEtatRassasie.obtenirInstance());
                 }
             }
         } else {
             tourEffrayer.put(animal, 2);
-            if (jeu.verifierCaseDecors(ligne, colonne)) {
-                int type = jeu.obtenirCaseDecors(ligne, colonne).obtenirType();
-                if(type == Acteur.TYPE_ARBRE){
-                    animal.changerEtat(new EcureuilAnimalEtatPerche(EcureuilAnimalEtatEffraye.obtenirInstance()));
-                } else if (type == Acteur.TYPE_BUISSON){
-                    animal.changerEtat(new EcureuilAnimalEtatCache(EcureuilAnimalEtatEffraye.obtenirInstance()));
-                }
+        }
+        if(animal.obtenirSaturation() > 0){
+            animal.changerSaturation(animal.obtenirSaturation() - 1);
+        }
+
+        if(jeu.verifierCaseDecors(ligne, colonne)){
+            int type = jeu.obtenirCaseDecors(ligne, colonne).obtenirType();
+            if(type == Acteur.TYPE_ARBRE){
+                animal.changerEtat(new EcureuilAnimalEtatPerche(animal.obtenirEtat()));
+            } else if (type == Acteur.TYPE_BUISSON){
+                animal.changerEtat(new EcureuilAnimalEtatCache(animal.obtenirEtat()));
             }
         }
     }
--- src/main/java/modele/HibouEtatVol.java
+++ src/main/java/modele/HibouEtatVol.java
@@ -31,7 +31,8 @@ public class HibouEtatVol extends HibouEtat {
                     aAttaque = true;
                     hibou.changerLigne(proie.obtenirLigne());
                     hibou.changerColonne(proie.obtenirColonne());
-
+                    System.out.println("aled");
+                    System.out.println(proie.obtenirEtat());
                     if (!proie.fuire(jeu, List.of(Acteur.TYPE_ARBRE, Acteur.TYPE_BUISSON))){
                         jeu.obtenirAnimaux().remove(proie);
                     }
